42f9c3725fbbc108741920d3b8a2ceea33305e50
Salman-sajib <sfsajib@gmail.com>
2024-01-15 14:54:06 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# React useRef()
+
+_<font color="green">In this small project I have learned about the useRef() react hook.</font>_
+
+### The differences between useState() and useRef() React hook
+
+### useState()
+
+- Re-renders the component when the state value changes.
+
+### useRef()
+
+- "use Reference" Does not cause any ==re-renders== when its value changes.
+- When you want a component to ==remember== some information but don't want that information to trigger new renders.
+
+### When useRef() is useful
+
+1. Accessing/Interacting with ==DOM== element
+2. Handling ==Focus==, ==Animation== and ==Transitions==
+3. Managing ==Timers== and ==Itervals==
+
+If you want to learn more about useRef() React hook follow those link below, It will take you to the official **ReactDOC** and an youTube video **tutorial**
+
+- [React DOC](https://react.dev/reference/react/useRef)
+
+- [Bro Code](https://youtu.be/AltU-XcGvuo?si=shPEN2whfDrspIAT) YouTube
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useRef } from 'react';
+
+function App() {
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+
+  useEffect(() => {
+    console.log('Component rendered');
+  });
+
+  function handleClick1() {
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = 'yellow';
+    inputRef2.current.style.backgroundColor = '';
+    inputRef3.current.style.backgroundColor = '';
+  }
+
+  function handleClick2() {
+    inputRef2.current.focus();
+    inputRef1.current.style.backgroundColor = '';
+    inputRef2.current.style.backgroundColor = 'yellow';
+    inputRef3.current.style.backgroundColor = '';
+  }
+
+  function handleClick3() {
+    inputRef3.current.focus();
+    inputRef1.current.style.backgroundColor = '';
+    inputRef2.current.style.backgroundColor = '';
+    inputRef3.current.style.backgroundColor = 'yellow';
+  }
+
+  return (
+    <>
+      <button onClick={handleClick1}>Click me!</button>
+      <input ref={inputRef1} type='text' />
+      <br />
+
+      <button onClick={handleClick2}>Click me!</button>
+      <input ref={inputRef2} type='text' />
+      <br />
+
+      <button onClick={handleClick3}>Click me!</button>
+      <input ref={inputRef3} type='text' />
+    </>
+  );
+}
+
+export default App;
